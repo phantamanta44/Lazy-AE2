67dac91f55a774d988d987c83ba8236b02ea7e5c
phantamanta44 <evandalong@gmail.com>
2019-05-20 00:16:03 -0500

    Fix machine inventory models

--- src/main/java/io/github/phantamanta44/threng/block/ThrEngBlocks.java
+++ src/main/java/io/github/phantamanta44/threng/block/ThrEngBlocks.java
@@ -1,6 +1,8 @@
 package io.github.phantamanta44.threng.block;
 
 import io.github.phantamanta44.libnine.InitMe;
+import io.github.phantamanta44.libnine.LibNine;
+import io.github.phantamanta44.threng.constant.LangConst;
 import io.github.phantamanta44.threng.constant.ThrEngConst;
 
 @SuppressWarnings("NullableProblems")
@@ -11,6 +13,12 @@ public class ThrEngBlocks {
     @InitMe(ThrEngConst.MOD_ID)
     public static void init() {
         MACHINE = new BlockMachine();
+
+        BlockMachine.Type[] machineTypes = BlockMachine.Type.values();
+        for (int i = 0; i < machineTypes.length; i++) {
+            LibNine.PROXY.getRegistrar()
+                    .queueItemBlockModelReg(MACHINE, i, LangConst.BLOCK_MACHINE, "inv_" + machineTypes[i].getName());
+        }
     }
 
 }
