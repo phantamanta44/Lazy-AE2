0fa399b6a93bf50ee2dbe64be3967d67f21f7fa0
phantamanta44 <evandalong@gmail.com>
2019-12-26 22:49:08 -0600

    Use object holders for registry entries

--- src/main/java/io/github/phantamanta44/threng/block/ThrEngBlocks.java
+++ src/main/java/io/github/phantamanta44/threng/block/ThrEngBlocks.java
@@ -1,18 +1,22 @@
 package io.github.phantamanta44.threng.block;
 
 import io.github.phantamanta44.libnine.InitMe;
+import io.github.phantamanta44.threng.constant.LangConst;
 import io.github.phantamanta44.threng.constant.ThrEngConst;
+import net.minecraftforge.fml.common.registry.GameRegistry;
 
 @SuppressWarnings("NotNullFieldNotInitialized")
 public class ThrEngBlocks {
 
+    @GameRegistry.ObjectHolder(ThrEngConst.MOD_ID + ":" + LangConst.BLOCK_MACHINE)
     public static BlockMachine MACHINE;
+    @GameRegistry.ObjectHolder(ThrEngConst.MOD_ID + ":" + LangConst.BLOCK_BIG_ASSEMBLER)
     public static BlockBigAssembler BIG_ASSEMBLER;
 
     @InitMe(ThrEngConst.MOD_ID)
     public static void init() {
-        MACHINE = new BlockMachine();
-        BIG_ASSEMBLER = new BlockBigAssembler();
+        new BlockMachine();
+        new BlockBigAssembler();
     }
 
 }
--- src/main/java/io/github/phantamanta44/threng/item/ThrEngItems.java
+++ src/main/java/io/github/phantamanta44/threng/item/ThrEngItems.java
@@ -1,16 +1,19 @@
 package io.github.phantamanta44.threng.item;
 
 import io.github.phantamanta44.libnine.InitMe;
+import io.github.phantamanta44.threng.constant.LangConst;
 import io.github.phantamanta44.threng.constant.ThrEngConst;
+import net.minecraftforge.fml.common.registry.GameRegistry;
 
 @SuppressWarnings("NotNullFieldNotInitialized")
 public class ThrEngItems {
 
+    @GameRegistry.ObjectHolder(ThrEngConst.MOD_ID + ":" + LangConst.ITEM_MATERIAL)
     public static ItemMaterial MATERIAL;
 
     @InitMe(ThrEngConst.MOD_ID)
     public static void init() {
-        MATERIAL = new ItemMaterial();
+        new ItemMaterial();
     }
 
 }
