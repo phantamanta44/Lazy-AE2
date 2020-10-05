1f08c91518d1d37d4d65648f4dce0a4e5eeee188
phantamanta44 <evandalong@gmail.com>
2020-10-05 11:41:25 -0500

    Optimize conjoined item handler for speed over memory

--- src/main/java/io/github/phantamanta44/threng/util/ConjoinedItemHandler.java
+++ src/main/java/io/github/phantamanta44/threng/util/ConjoinedItemHandler.java
@@ -5,21 +5,29 @@ import net.minecraftforge.items.IItemHandler;
 import net.minecraftforge.items.IItemHandlerModifiable;
 
 import java.util.Arrays;
-import java.util.List;
+import java.util.Collection;
 
+// faster lookup but higher memory overhead than forge's CombinedInvWrapper
 public class ConjoinedItemHandler implements IItemHandlerModifiable {
 
-    private final List<IItemHandler> delegates;
-    private final int[] thresholds;
+    private final IItemHandler[] delegates;
+    private final int[] baseIndices;
 
-    public ConjoinedItemHandler(List<IItemHandler> delegates) {
-        this.delegates = delegates;
-        this.thresholds = new int[delegates.size()];
-        if (!delegates.isEmpty()) {
-            thresholds[0] = delegates.get(0).getSlots();
-            for (int i = 1; i < thresholds.length; i++) {
-                thresholds[i] = thresholds[i - 1] + delegates.get(i).getSlots();
+    public ConjoinedItemHandler(Collection<IItemHandler> delegates) {
+        int i = 0;
+        for (IItemHandler delegate : delegates) {
+            i += delegate.getSlots();
+        }
+        this.delegates = new IItemHandler[i];
+        this.baseIndices = new int[i];
+        i = 0;
+        for (IItemHandler delegate : delegates) {
+            int slotCount = delegate.getSlots();
+            for (int j = 0; j < slotCount; j++) {
+                this.delegates[i + j] = delegate;
+                this.baseIndices[i + j] = i;
             }
+            i += slotCount;
         }
     }
 
@@ -29,63 +37,40 @@ public class ConjoinedItemHandler implements IItemHandlerModifiable {
 
     @Override
     public int getSlots() {
-        return delegates.stream().mapToInt(IItemHandler::getSlots).sum();
+        return delegates.length;
     }
 
     @Override
     public ItemStack getStackInSlot(int slot) {
-        int ihIndex = getItemHandler(slot);
-        return delegates.get(ihIndex).getStackInSlot(slot - getBaseSlot(ihIndex));
+        return delegates[slot].getStackInSlot(slot - baseIndices[slot]);
     }
 
     @Override
     public void setStackInSlot(int slot, ItemStack stack) {
-        int ihIndex = getItemHandler(slot);
-        IItemHandler inv = delegates.get(ihIndex);
+        IItemHandler inv = delegates[slot];
         if (inv instanceof IItemHandlerModifiable) {
-            ((IItemHandlerModifiable)inv).setStackInSlot(slot - getBaseSlot(ihIndex), stack);
+            ((IItemHandlerModifiable)inv).setStackInSlot(slot - baseIndices[slot], stack);
         }
     }
 
     @Override
     public ItemStack insertItem(int slot, ItemStack stack, boolean simulate) {
-        int ihIndex = getItemHandler(slot);
-        return delegates.get(ihIndex).insertItem(slot - getBaseSlot(ihIndex), stack, simulate);
+        return delegates[slot].insertItem(slot - baseIndices[slot], stack, simulate);
     }
 
     @Override
     public ItemStack extractItem(int slot, int amount, boolean simulate) {
-        int ihIndex = getItemHandler(slot);
-        return delegates.get(ihIndex).extractItem(slot - getBaseSlot(ihIndex), amount, simulate);
+        return delegates[slot].extractItem(slot - baseIndices[slot], amount, simulate);
     }
 
     @Override
     public int getSlotLimit(int slot) {
-        int ihIndex = getItemHandler(slot);
-        return delegates.get(ihIndex).getSlotLimit(slot - getBaseSlot(ihIndex));
+        return delegates[slot].getSlotLimit(slot - baseIndices[slot]);
     }
 
     @Override
     public boolean isItemValid(int slot, ItemStack stack) {
-        int ihIndex = getItemHandler(slot);
-        return delegates.get(ihIndex).isItemValid(slot - getBaseSlot(ihIndex), stack);
-    }
-
-    private int getItemHandler(int index) {
-        for (int i = 0; i < thresholds.length; i++) {
-            if (index < thresholds[i]) {
-                return i;
-            }
-        }
-        int slotCount = getSlots();
-        if (slotCount == 0) {
-            throw new IndexOutOfBoundsException(String.format("%d is not a slot in an empty inventory!", index));
-        }
-        throw new IndexOutOfBoundsException(String.format("%d is not in slot indices [0, %d)!", index, slotCount));
-    }
-
-    private int getBaseSlot(int ihIndex) {
-        return ihIndex == 0 ? 0 : thresholds[ihIndex - 1];
+        return delegates[slot].isItemValid(slot - baseIndices[slot], stack);
     }
 
 }
