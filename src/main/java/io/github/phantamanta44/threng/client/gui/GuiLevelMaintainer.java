a72eaf7d8993a61b8827b00f1814fd423a291f24
Illia Halatiuk <galatyuk.ilya@gmail.com>
2021-11-28 15:15:20 +0200

    Remove number in slot

--- src/main/java/io/github/phantamanta44/threng/client/gui/GuiLevelMaintainer.java
+++ src/main/java/io/github/phantamanta44/threng/client/gui/GuiLevelMaintainer.java
@@ -1,17 +1,12 @@
 package io.github.phantamanta44.threng.client.gui;
 
-import appeng.container.slot.SlotFake;
-import appeng.util.ReadableNumberConverter;
 import io.github.phantamanta44.libnine.client.gui.L9GuiContainer;
 import io.github.phantamanta44.libnine.client.gui.component.impl.GuiComponentTextInput;
-import io.github.phantamanta44.libnine.util.render.GuiUtils;
 import io.github.phantamanta44.threng.constant.LangConst;
 import io.github.phantamanta44.threng.constant.ResConst;
 import io.github.phantamanta44.threng.inventory.ContainerLevelMaintainer;
 import io.github.phantamanta44.threng.tile.TileLevelMaintainer;
-import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.resources.I18n;
-import net.minecraft.inventory.Slot;
 
 import java.util.function.LongConsumer;
 
@@ -67,26 +62,4 @@ public class GuiLevelMaintainer extends L9GuiContainer {
         super.drawForeground(partialTicks, mX, mY);
         drawContainerName(I18n.format(LangConst.CONTAINER_LEVEL_MAINTAINER));
     }
-
-    @Override
-    public void drawSlot(Slot slot) {
-        super.drawSlot(slot);
-        if (slot instanceof SlotFake) {
-            long count = cont.getRequestQuantity(slot.getSlotIndex());
-            if (count > 0) {
-                String countStr = ReadableNumberConverter.INSTANCE.toSlimReadableForm(count);
-                GlStateManager.pushMatrix();
-                GlStateManager.disableDepth();
-                GlStateManager.scale(0.75F, 0.75F, 0.75F);
-                drawString(countStr,
-                        (int)Math.floor((slot.xPos + 16 - GuiUtils.getStringWidth(countStr) * 0.375F) * 1.333F),
-                        (int)Math.floor((slot.yPos + 16 - GuiUtils.getFontHeight() * 0.375F) * 1.333F),
-                        0xFFFFFF, true);
-                GlStateManager.color(1F, 1F, 1F, 1F);
-                GlStateManager.enableDepth();
-                GlStateManager.popMatrix();
-            }
-        }
-    }
-
 }
